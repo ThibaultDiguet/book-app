937f204c80138fd90a157fadfa7ce066cc20640f
T.Diguet <t.diguet@rcontact.fr>
2022-06-24 16:59:31 +0200

    build version

--- BookAPI/webpack.config.js
+++ BookAPI/webpack.config.js
@@ -7,20 +7,22 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
+    .enableReactPreset()
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
+    .setManifestKeyPrefix('build/')
+    
     /*
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('mainjs', '../BookApp/build/static/js/main.6ceed0b4.js')
+    .addStyleEntry('main', '../BookApp/build/static/css/main.6759d59c.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
